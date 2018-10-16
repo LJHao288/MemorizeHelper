42941e2a63ae1a5f18862458b416836bc5e1cefe
LJHao288 <LJHao288@users.noreply.github.com>
2018-10-16 17:18:32 +0200

    Initial Commit

---
+++ MemorizeHelper.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using MemorizeHelper.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemorizeHelper.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
