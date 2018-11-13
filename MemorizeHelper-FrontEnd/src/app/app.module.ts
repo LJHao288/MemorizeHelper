bcc7fe9f1bef98f60560d086fcbe22553124eaa7
abdullahalzoabi <abdullahalzoabi.zo@gmail.com>
2018-11-13 15:01:17 +0200

    Add the empty "Edit Unit " component to front end

--- MemorizeHelper-FrontEnd/src/app/app.module.ts
+++ MemorizeHelper-FrontEnd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { AddMemoryUitComponent } from './add-memory-uit/add-memory-uit.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
 import { ViewUnitComponent } from './view-unit/view-unit.component';
+import { EditUnitComponent } from './edit-unit/edit-unit.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ViewUnitComponent } from './view-unit/view-unit.component';
     AddMemoryUitComponent,
     UserprofileComponent,
     ViewUnitComponent,
+    EditUnitComponent,
     
   ],
   imports: [
