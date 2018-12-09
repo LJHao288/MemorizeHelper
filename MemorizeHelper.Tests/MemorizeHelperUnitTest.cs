b2b4a40ec9c236dfcc5e72d25cd48f684f6bafec
LJHao288 <LJHao288@users.noreply.github.com>
2018-12-09 21:30:00 +0100

    Unit test Finished

--- MemorizeHelper.Tests/MemorizeHelperUnitTest.cs
+++ MemorizeHelper.Tests/MemorizeHelperUnitTest.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MemorizeHelper.Tests
 {
@@ -73,75 +75,113 @@ namespace MemorizeHelper.Tests
             Assert.IsType<StatusCodeResult>(result);
         }
 
+        private List<MemorizeUnit> GetTestMemorizeUnit(){
+            var MemorizeUnits = new List<MemorizeUnit>();
+            MemorizeUnits.Add(new MemorizeUnit(){
+                Id=1,
+                OwnerUsername ="li",
+                Title="Verify rule",
+                StringContent = "abc",
+                SubjectName = "Program",
+                IsPrivate = false,
+                CreationDate = new DateTime(2018,11,15,11,0,0),
+                Priority = "1",
+                Tags = "T"
 
+            });
 
-        [Fact]
-        public async void Get()
-        {
-            //Arrange
+            MemorizeUnits.Add(new MemorizeUnit(){
+                Id=1,
+                OwnerUsername ="li",
+                Title="rule",
+                StringContent = "abc",
+                SubjectName = "Program",
+                IsPrivate = false,
+                CreationDate = new DateTime(2018,11,15,11,0,0),
+                Priority = "1",
+                Tags = "T"
 
-            //Act
+            });
 
-            //Assert
+            MemorizeUnits.Add(new MemorizeUnit(){
+                Id=1,
+                OwnerUsername ="li",
+                Title="rule",
+                StringContent = "abc",
+                SubjectName = "Program",
+                IsPrivate = false,
+                CreationDate = new DateTime(2018,11,15,11,0,0),
+                Priority = "1",
+                Tags = "T"
+
+            });
+
+            MemorizeUnits.Add(new MemorizeUnit(){
+                Id=1,
+                OwnerUsername ="li",
+                Title="VR",
+                StringContent = "abc",
+                SubjectName = "Program",
+                IsPrivate = false,
+                CreationDate = new DateTime(2018,11,15,11,0,0),
+                Priority = "1",
+                Tags = "T"
+
+            });
+            return MemorizeUnits;
         }
-
-        [Fact]
-        public async void GetSchedule()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
-
 
         [Fact]
         public async void GetPopular()
         {
             //Arrange
-
+            var maxNum = 5;
+            DataContext db = new DataContext();
+            var mockRepo = new Mock<IMemorizeHelperRepository>();
+            mockRepo.Setup(repo => repo.GetPopularMemorizeUnits(maxNum))
+                .ReturnsAsync(GetTestMemorizeUnit());
+            var controller = new MemorizeUnitController(mockRepo.Object,db);
             //Act
-
+            var result = await controller.GetPopular(maxNum);
             //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<MemorizeUnit>>(okResult.Value);
+            var firstMemorizeUnit = returnValue.FirstOrDefault();
+            Assert.Equal("Verify rule",firstMemorizeUnit.Title);
+            Assert.Equal(4,returnValue.Count());
         }
 
-        [Fact]
-        public async void GetAllPublic()
-        {
-            //Arrange
+        
 
-            //Act
 
-            //Assert
+        private PagedList<MemorizeUnit> GetTestPagedMemorizeUnit(){
+            var pagedMemorizeUnit = new PagedList<MemorizeUnit>(GetTestMemorizeUnit().Take(2).ToList(),4,1,2);
+            return pagedMemorizeUnit;
         }
 
-        [Fact]
-        public async void GetForUserInPage()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
-        [Fact]
-        public async void GetReviewTaskToday()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
         [Fact]
         public async void Copy()
         {
             //Arrange
-
+            var MemorrizeUnitId = 2;
+            var userName = "huang";
+            DataContext db = new DataContext();
+            var mockRepo = new Mock<IMemorizeHelperRepository>();
+            mockRepo.Setup(repo => repo.GetUserByUsername(userName))
+                .ReturnsAsync(new User{Username ="huang"});
+            mockRepo.Setup(repo => repo.GetMemorizeUnitNoTracking(MemorrizeUnitId))
+                .ReturnsAsync(GetTestMemorizeUnit().FirstOrDefault());
+            mockRepo.Setup(repo => repo.GetCounterUnitByMemorizeUnitId(MemorrizeUnitId))
+                .ReturnsAsync(new CounterUnit{Id=1, MemorizeUnitId=2,Count=4});
+            mockRepo.Setup(repo => repo.SaveAll())
+                .ReturnsAsync(true);
+            var controller = new MemorizeUnitController(mockRepo.Object,db);
             //Act
-
+            var result = await controller.Copy(2,"huang");
             //Assert
+            Assert.IsType<OkResult>(result);
         }
+
+
     }
 }
